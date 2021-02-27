f84bce7dfdda7e77d7ee596850341ccbdb02422d
Lorand-F <formagiu.lorand@outlook.com>
2021-02-27 11:22:13 +0200

    adaugare clase gradina zoologica

---
+++ seminar1/src/clase/WildCat.java
@@ -0,0 +1,21 @@
+package clase;
+
+public class WildCat extends Animal{
+
+	public WildCat(String nume) {
+		super(nume);
+		// TODO Auto-generated constructor stub
+	}
+
+	private String mancare;
+
+	public WildCat(String nume, String mancare) {
+		super(nume);
+		this.mancare = mancare;
+	}
+	
+	
+	
+	
+	
+}
--- seminar1/src/seminar1/Main.java
+++ seminar1/src/seminar1/Main.java
@@ -2,6 +2,7 @@ package seminar1;
 
 import clase.Animal;
 import clase.Lion;
+import clase.WildCat;
 import clase.Zoo;
 
 public class Main {
@@ -12,6 +13,12 @@ public class Main {
 		Animal lion2=new Lion("Leona");
 		zoo.addAnimal(lion1);
 		zoo.addAnimal(lion2);
+		zoo.feedAllAnimals();
+		
+		WildCat cat=new WildCat("Pisica1");
+		WildCat cat2=new WildCat("Pisica2","Mancare2");
+		zoo.addAnimal(cat);
+		zoo.addAnimal(cat2);
 		zoo.feedAllAnimals();
 	}
 	
