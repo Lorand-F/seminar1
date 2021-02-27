8e06a63b9fb1a01fd4ac8b8bd314708614c9579e
Lorand-F <formagiu.lorand@outlook.com>
2021-02-27 11:12:39 +0200

    # WARNING: head commit changed in the meantime
    incercare

--- seminar1/src/seminar1/Main.java
+++ seminar1/src/seminar1/Main.java
@@ -1,7 +1,19 @@
 package seminar1;
 
+import clase.Animal;
+import clase.Lion;
+import clase.Zoo;
+
 public class Main {
+	
 	public static void main(String[] args) {
-		
+		Zoo zoo=new Zoo();
+		Animal lion1=new Lion("Leo");
+		Animal lion2=new Lion("Leona");
+		zoo.addAnimal(lion1);
+		zoo.addAnimal(lion2);
+		zoo.feedAllAnimals();
 	}
+	
+	
 }
